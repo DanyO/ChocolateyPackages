a50bea30229ebf075faafdb4c9190ca35526f207
DanyO <me@danyo.ca>
2014-06-26 18:57:57 -0300

    Corrected s3browser and litecoin packages

--- s3browser/tools/chocolateyInstall.ps1
+++ s3browser/tools/chocolateyInstall.ps1
@@ -1,15 +1,8 @@
-$packageName = 's3browser' # arbitrary name for the package, used in messages
-$fileType = 'exe'
-$url = 'http://s3browser.com/download/s3browser-4-4-5.exe'
-try
-{
-	#Install-ChocolateyPackage $packageName $fileType $silentArgs $url
-    Install-ChocolateyPackage $packageName $fileType '/silent' $url
+﻿$packageName = 's3browser' 			# arbitrary name for the package, used in messages
+$installerType = 'EXE' 				# only one of these: exe, msi, msu
+$url = 'http://s3browser.com/download/s3browser-4-4-5.exe' 					# download url
+$url64 = $url 						# 64bit URL here or remove - if installer decides, then use $url
+$silentArgs = '/silent' 			# "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$validExitCodes = @(0) 				# please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-    Write-ChocolateySuccess $packageName
-}
-catch
-{
-    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-    throw 
-}
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
--- skitch/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$packageName = 'skitch' 			# arbitrary name for the package, used in messages
-$installerType = 'EXE' 	#only one of these: exe, msi, msu
-$url = 'http://evernote.com/download/get.php?file=SkitchWindows' 					# download url
-$url64 = $url 						# 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = '--unattendedmodeui none --mode unattended' 	# "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) 				#please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"
