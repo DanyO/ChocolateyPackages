75a429a0d20cc1f824687f4da661807095611e85
DanyO <me@danyo.ca>
2014-07-13 12:04:02 -0300

    Add latest beta build of Beyond COmpare 4

--- beyondcompare4/tools/chocolateyInstall.ps1
+++ beyondcompare4/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'beyondcompare4' 	# arbitrary name for the package, used in messages
 $installerType = 'EXE' 				#only one of these: exe, msi, msu
-$url = 'http://www.scootersoftware.com/BCompare-4.0.0.18291.exe' 					# download url
+$url = 'http://www.scootersoftware.com/BCompare-4.0.0.18509.exe' 					# download url
 $url64 = $url 						# 64bit URL here or remove - if installer decides, then use $url
 $silentArgs = '/SP- /VERYSILENT /NORESTART' 	# "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 
